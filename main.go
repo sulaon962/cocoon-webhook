701f8f36cfa707c0912d4d77fd2978785fac4c05
CMGS <ilskdw@gmail.com>
2026-04-24 00:37:04 +0800

    adopt cocoon-common httpx and NewClientset helpers

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"crypto/tls"
-	"errors"
 	"net/http"
 	"os/signal"
 	"syscall"
@@ -11,8 +10,8 @@ import (
 
 	"github.com/projecteru2/core/log"
 	"github.com/prometheus/client_golang/prometheus"
-	"k8s.io/client-go/kubernetes"
 
+	commonhttpx "github.com/cocoonstack/cocoon-common/httpx"
 	commonk8s "github.com/cocoonstack/cocoon-common/k8s"
 	commonlog "github.com/cocoonstack/cocoon-common/log"
 	"github.com/cocoonstack/cocoon-webhook/admission"
@@ -25,6 +24,8 @@ const (
 	defaultKeyFile       = "/etc/cocoon/webhook/certs/tls.key"
 	defaultListen        = ":8443"
 	defaultMetricsListen = ":9090"
+
+	shutdownTimeout = 15 * time.Second
 )
 
 func main() {
@@ -45,62 +46,32 @@ func main() {
 		logger.Fatalf(ctx, err, "load TLS keypair: %v", err)
 	}
 
-	kubeConfig, err := commonk8s.LoadConfig()
-	if err != nil {
-		logger.Fatalf(ctx, err, "load kubeconfig: %v", err)
-	}
-	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	clientset, err := commonk8s.NewClientset()
 	if err != nil {
 		logger.Fatalf(ctx, err, "build clientset: %v", err)
 	}
 
-	server := &http.Server{
-		Addr:              listen,
-		Handler:           admission.NewServer(clientset).Routes(),
-		ReadHeaderTimeout: 10 * time.Second,
-		TLSConfig: &tls.Config{
-			Certificates: []tls.Certificate{cert},
-			MinVersion:   tls.VersionTLS12,
-		},
+	webhookServer := commonhttpx.NewServer(listen, admission.NewServer(clientset).Routes())
+	webhookServer.TLSConfig = &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		MinVersion:   tls.VersionTLS12,
 	}
-
-	// Capture root ctx before signal.NotifyContext shadows it; shutdown must
-	// outlive the signal-derived ctx (which is canceled the moment we handle SIGINT/SIGTERM).
-	rootCtx := ctx
-	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
 
 	metricsMux := http.NewServeMux()
 	metricsMux.Handle("/metrics", metrics.Handler())
-	metricsServer := &http.Server{
-		Addr:              metricsListen,
-		Handler:           metricsMux,
-		ReadHeaderTimeout: 10 * time.Second,
-	}
-	go func() {
-		logger.Infof(ctx, "cocoon-webhook metrics listening on %s", metricsListen)
-		if serveErr := metricsServer.ListenAndServe(); serveErr != nil && !errors.Is(serveErr, http.ErrServerClosed) {
-			logger.Error(ctx, serveErr, "metrics listen and serve")
-			cancel()
-		}
-	}()
+	metricsServer := commonhttpx.NewServer(metricsListen, metricsMux)
 
-	go func() {
-		logger.Infof(ctx, "cocoon-webhook %s started (rev=%s built=%s) on %s",
-			version.VERSION, version.REVISION, version.BUILTAT, listen)
-		if serveErr := server.ListenAndServeTLS("", ""); serveErr != nil && !errors.Is(serveErr, http.ErrServerClosed) {
-			logger.Error(ctx, serveErr, "listen and serve")
-			cancel()
-		}
-	}()
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
 
-	<-ctx.Done()
-	shutdownCtx, shutdownCancel := context.WithTimeout(rootCtx, 15*time.Second)
-	defer shutdownCancel()
-	if err := server.Shutdown(shutdownCtx); err != nil {
-		logger.Warnf(shutdownCtx, "shutdown admission: %v", err)
+	logger.Infof(ctx, "cocoon-webhook %s started (rev=%s built=%s) on %s (metrics on %s)",
+		version.VERSION, version.REVISION, version.BUILTAT, listen, metricsListen)
+
+	specs := []commonhttpx.ServerSpec{
+		commonhttpx.HTTPSServerSpec(webhookServer, "", ""),
+		commonhttpx.HTTPServerSpec(metricsServer),
 	}
-	if err := metricsServer.Shutdown(shutdownCtx); err != nil {
-		logger.Warnf(shutdownCtx, "shutdown metrics: %v", err)
+	if err := commonhttpx.Run(ctx, shutdownTimeout, specs...); err != nil {
+		logger.Errorf(ctx, err, "run servers")
 	}
 }
